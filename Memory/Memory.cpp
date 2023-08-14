fdf7a25e1435fa6993fb182cb90397b7b1253938
Egor Bobrikov <egor.bobrikov@xa.fracturedbyte.com>
2023-08-14 21:58:03 +0300

    Add string example

--- Memory/Memory.cpp
+++ Memory/Memory.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <string>
 #include "CustomAllocator.h"
 #include "CustomSharedPtr.h"
 
@@ -7,6 +8,7 @@ using namespace std;
 
 int main()
 {
+	cout << "Custom allocator block ================================" << endl;
 	vector<int, CustomAllocator<int>> vec;
 	vec.push_back(12);
 	vec.push_back(345);
@@ -18,30 +20,39 @@ int main()
 		cout << val << endl;
 	}
 	
+	vector<string, CustomAllocator<string>> strVec;
+	strVec.push_back("Hello");
+	strVec.push_back("from");
+	strVec.push_back("strings");
+
+	for (auto val : strVec)
+	{
+		cout << val << endl;
+	}
+
+	cout << "Custom shared pointer block ================================" << endl;
+	if(1)
+	{
+		int* value = new int(10);
+		// Create shared ptr 
+		CustomSharedPtr<int> myPtr = CustomSharedPtr<int>(value);
+		cout << *myPtr << endl;
+		cout << "================================" << endl;
+		{
+			CustomSharedPtr<int> myPtr1 = myPtr;
+			cout << *myPtr << endl;
+			cout << *myPtr1 << endl;
+			{
+				CustomSharedPtr<int> myPtr2 = myPtr1;
+				cout << *myPtr << endl;
+				cout << *myPtr1 << endl;
+				cout << *myPtr2 << endl;
+			}
 
 
-	//if(1)
-	//{
-	//	int* value = new int(10);
-	//	// Create shared ptr 
-	//	CustomSharedPtr<int> myPtr = CustomSharedPtr<int>(value);
-	//	cout << *myPtr << endl;
-	//	cout << "================================" << endl;
-	//	{
-	//		CustomSharedPtr<int> myPtr1 = myPtr;
-	//		cout << *myPtr << endl;
-	//		cout << *myPtr1 << endl;
-	//		{
-	//			CustomSharedPtr<int> myPtr2 = myPtr1;
-	//			cout << *myPtr << endl;
-	//			cout << *myPtr1 << endl;
-	//			cout << *myPtr2 << endl;
-	//		}
-
-
-	//	}
-	//}
-	//cout << "================================" << endl;
+		}
+	}
+	cout << "================================" << endl;
 	
 
 
